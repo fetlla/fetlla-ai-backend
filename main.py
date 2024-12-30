67f2aafb1776c83eb7c06d3eba1ff95256ec1c2d
Luth3r <aravindor@gmail.com>
2024-12-30 13:43:40 +0530

    Custom pydantic exception added

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
 from routers import auth
 from models import Base
 from database import engine
@@ -7,7 +10,14 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request:Request,exc:RequestValidationError):
+    first_error = exc.errors()[0] if exc.errors() else {}
+    error_message = first_error.get('msg','Validation Error')
+    error_field = first_error.get('loc', [''])[1] if len(first_error.get('loc', [])) > 1 else ''
+    return JSONResponse(status_code=400,content={"message":f"{error_field} {error_message}"})
+
 
 @app.get("/")
 async def home():
-    return {"message": "LLM project"}
+    return {"message": "visit /docs for Swagger"}
